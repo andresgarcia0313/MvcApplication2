1c08d4052c06a57a5e5fabd2a14b32eea1f46854
Andres Eduardo Garcia Marquez <andresgarcia0313@gmail.com>
2019-08-15 19:25:47 -0500

    Agregar archivos de proyecto.

---
+++ MvcApplication2/Controllers/PersonaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication2.Models;
+
+namespace MvcApplication2.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            List<Persona> listaPersona = new List<Persona>();
+            Persona ObjPersona = new Persona
+            {
+                Codigo = 1,
+                Nombre = "Andrés Eduardo",
+                ApellidoPaterno = "García",
+                ApellidoMaterno = "Márquez"
+            };
+            Persona ObjPersona1 = new Persona
+            {
+                Codigo = 2,
+                Nombre = "Katherin",
+                ApellidoPaterno = "Usa",
+                ApellidoMaterno = "Patiño"
+            };
+            Persona ObjPersona2 = new Persona
+            {
+                Codigo = 3,
+                Nombre = "Yezny Yormary",
+                ApellidoPaterno = "García",
+                ApellidoMaterno = "Márquez"
+            };
+            listaPersona.Add(ObjPersona);
+            listaPersona.Add(ObjPersona1);
+            listaPersona.Add(ObjPersona2);
+            return View(listaPersona);
+        }
+    }
+}
